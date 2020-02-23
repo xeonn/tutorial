977c2991331c77b666c50be760477b009f96f0b9
Onn Khairuddin Ismail <onn.khairuddin@gmail.com>
2020-02-23 12:50:11 +0800

    Initial import - Springboot with Keycloak
    Based on https://www.baeldung.com/spring-boot-keycloak

---
+++ Spring-Keycloak/README.md
@@ -0,0 +1,4 @@
+Springboot With Keycloak Tutorial
+
+Based on https://www.baeldung.com/spring-boot-keycloak
+
---
+++ Spring-Keycloak/src/test/java/my/onn/sb/kc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package my.onn.sb.kc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
