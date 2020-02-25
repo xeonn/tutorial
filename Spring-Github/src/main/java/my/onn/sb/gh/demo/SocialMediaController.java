e7dfb13e8ba7a53ca3ed3712448142eb01a94bf6
Onn Khairuddin Ismail <onn.khairuddin@gmail.com>
2020-02-25 14:14:08 +0800

    Added error handling for sb for github/google oauth2.

--- Spring-Github/src/main/java/my/onn/sb/gh/demo/SocialMediaController.java
+++ Spring-Github/src/main/java/my/onn/sb/gh/demo/SocialMediaController.java
@@ -2,6 +2,7 @@ package my.onn.sb.gh.demo;
 
 import java.util.Collections;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +15,12 @@ public class SocialMediaController {
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         System.out.println("Logged in as " + principal.getName());
         return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
     }
 }
--- Spring-Github/src/main/java/my/onn/sb/gh/demo/WebConfigurer.java
+++ Spring-Github/src/main/java/my/onn/sb/gh/demo/WebConfigurer.java
@@ -1,15 +1,20 @@
 package my.onn.sb.gh.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebConfigurer extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    AuthenticationFailureHandler handler;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //@formatter:off
@@ -22,7 +27,10 @@ public class WebConfigurer extends WebSecurityConfigurerAdapter {
                 .exceptionHandling(e -> e
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login();
+                .oauth2Login(o -> o.failureHandler((req, res, ex) -> {
+                    req.getSession().setAttribute("error.message", ex.getMessage());
+                    handler.onAuthenticationFailure(req, res, ex);
+                }) );
         //@formatter:on
     }
 
