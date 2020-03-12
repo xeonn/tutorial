ddaa06588479b830d0553f1228fb97defc525f7f
Onn Khairuddin Ismail <onn.khairuddin@gmail.com>
2020-03-12 11:28:06 +0800

    Begin Springboot with Google Cloud Platform tutorial.

---
+++ Spring-GCP/src/main/java/com/jjv/tutorial/gcp/Springboot/with/GCP/SpringbootWithGcpApplication.java
@@ -0,0 +1,13 @@
+package com.jjv.tutorial.gcp.Springboot.with.GCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithGcpApplication.class, args);
+	}
+
+}
---
+++ Spring-GCP/src/test/java/com/jjv/tutorial/gcp/Springboot/with/GCP/SpringbootWithGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjv.tutorial.gcp.Springboot.with.GCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
